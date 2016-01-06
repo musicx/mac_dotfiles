fb134684520eb5cbacc65653dafab97d0447b821
mingxian.lyj <mingxian.lyj@alipay.com>
2016-01-06 10:33:30 +0800

    ali env alias change

--- zsh_custom/ali.zsh
+++ zsh_custom/ali.zsh
@@ -1,7 +1,7 @@
 export ALIEXT1="mingxian.lyj@100.81.137.24"
 export ALIEXT2="mingxian.lyj@100.81.137.25"
-alias ssh24='ssh mingxian.lyj@100.81.137.24'
-alias ssh25='ssh mingxian.lyj@100.81.137.25'
+alias ali24='ssh mingxian.lyj@100.81.137.24'
+alias ali25='ssh mingxian.lyj@100.81.137.25'
 
 export BDE1="mingxian.lyj@bde-1.inc.alipay.net"
 export BDE4="mingxian.lyj@bde-4.inc.alipay.net"
