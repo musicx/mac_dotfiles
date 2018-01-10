87e1a2a5e0c2fed5aa74f701e7761537efa0f312
LiuYijie <yijie.liu@ele.me>
2018-01-10 16:42:03 +0800

    some env change

--- zsh_custom/eleme.zsh
+++ zsh_custom/eleme.zsh
@@ -1,1 +1,2 @@
 alias goalpha='ssh master@10.104.111.33'
+alias copyalpha='sftp master@10.104.111.33'
--- zsh_custom/env.zsh
+++ zsh_custom/env.zsh
@@ -7,9 +7,8 @@
 # dev tools
 #export C_INCLUDE_PATH=/usr/local/opt/curl/include
 #export CPLUS_INCLUDE_PATH=/usr/local/opt/curl/include
-export JAVA_HOME=$(/usr/libexec/java_home)
+export JAVA_HOME=$(/usr/libexec/java_home --version 1.8)
 
-alias use_conda='export PATH=$HOME/Conda3/bin:$PATH'
 alias mdplus='open ~/Work/Languages/js/packages/markdown-plus/index.zh_CN.html'
 alias mdses='(export PORT=5000 && node ~/Work/Languages/js/packages/stackedit/server.js) &'
 alias mdse='open -a "Safari" "http://localhost:5000/"'
@@ -32,7 +31,10 @@ export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 #export PYLEARN2_DATA_PATH=$HOME/Work/Python/PyLearn2/data
 export AUTOJUMP_IGNORE_CASE=1
 export CHEATCOLORS=true
-export PYENV=$HOME/Work/Python/virtual
+
+# Python
+export PYTHONIOENCODING="utf8"
+export PYENV=$HOME/Code/Python/virtual
 
 #eval $(docker-machine env default)
 
